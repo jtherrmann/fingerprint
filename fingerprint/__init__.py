bbf261bc946628efe252e42193302b7371e42409
Jake Herrmann <jakeherrmann@posteo.net>
2019-09-20 07:08:12 -0800

    Add home.html and placeholder /fingerprint handler

--- fingerprint/__init__.py
+++ fingerprint/__init__.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return 'Welcome!'
+    return render_template('home.html')
+
+
+@app.route('/fingerprint')
+def fingerprint():
+    return 'TODO: your fingerprint here'
