7b6bbb22f62dac0906e1010ff1dbd0b57c91c004
Jake Herrmann <jakeherrmann@posteo.net>
2019-11-19 23:03:21 -0900

    Add button to hide/show all stats tables

--- fingerprint/static/stats.js
+++ fingerprint/static/stats.js
@@ -10,9 +10,26 @@ function statsButtonHandler() {
 }
 
 
+function statsToggleAll() {
+    let tables = $('.stats-table');
+    if (this.innerHTML == 'Hide all') {
+        for (let i = 0; i < tables.length; i++) {
+            tables[i].style.display = 'none';
+        }
+        this.innerHTML = 'Show all';
+    } else {
+        for (let i = 0; i < tables.length; i++) {
+            tables[i].style.display = 'block';
+        }
+        this.innerHTML = 'Hide all';
+    }
+}
+
+
 $(document).ready(function() {
     let buttons = $('.stats-button');
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].onclick = statsButtonHandler;
     }
+    $('#stats-toggle-all')[0].onclick = statsToggleAll;
 });
